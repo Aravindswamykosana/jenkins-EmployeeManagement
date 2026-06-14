a6fd8fd98ae0e8119d7c8d29a4a36667fcfa5123
Aravind Swamy <152840671+Aravindswamykosana@users.noreply.github.com>
2026-06-14 13:52:57 +0530

    Update register-service.ts

--- frontend/register/src/app/service/register-service.ts
+++ frontend/register/src/app/service/register-service.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class RegisterService {
 
   http=inject(HttpClient)
-  url = "http://localhost:8081/register";
+  url = "http://32.236.43.213:8081/register";
   
   saveUser(reg:Register1):Observable<Register1>{
     return this.http.post<Register1>(this.url,reg);
