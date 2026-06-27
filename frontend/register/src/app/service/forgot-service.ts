acbdc961371db46191fdab26a420df3c5b72ca6d
Aravind Swamy <152840671+Aravindswamykosana@users.noreply.github.com>
2026-06-27 18:37:53 +0530

    Update forgot-service.ts

--- frontend/register/src/app/service/forgot-service.ts
+++ frontend/register/src/app/service/forgot-service.ts
@@ -10,6 +10,6 @@ export class ForgotService {
   http=inject(HttpClient)
 
     forgotPwd(forgot:ForgotPwd1):Observable<ForgotPwd1>{
-      return this.http.post<ForgotPwd1>(`http://3.26.147.40:8081/forgotPwd?email=${forgot.email}`,null);
+      return this.http.post<ForgotPwd1>(`http://3.27.43.34:8081/forgotPwd?email=${forgot.email}`,null);
     }
 }
